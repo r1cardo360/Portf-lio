b414fa710c4cb93fae57ded0c08dc6b95408488d
Ricardo <100629700+r1cardo360@users.noreply.github.com>
2024-07-02 15:06:06 -0300

    Adicionando o arquivo JavaScript para puxar como função o footer e o nav .html

---
+++ Script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('nav.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('nav-placeholder').innerHTML = data;
+        });
+
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer-placeholder').innerHTML = data;
+        });
+});
